5bd603491adaadd00cbccf5ee1d6397c2ce48cc6
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-16 11:38:51 +0100

    creation des fichiers du projets

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome</title>
+</head>
+<body>
+    <header> 
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="inscription.php">News</a></li>
+                <li><a href="connexion.php">Contact</a></li>
+                <li><a href="profil.php">About</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+
+        </section>
+    </main>
+</body>
+</html>
